f4cadc1c98f8181d5a33d0661b44cceaa3148fc7
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-04-05 02:57:18 +0900

    add custom md renderer

--- src/pages/MdEditor/index.tsx
+++ src/pages/MdEditor/index.tsx
@@ -2,11 +2,61 @@ import React, { useState } from "react";
 import "./style.scss";
 import MdTextarea from "components/MdTextarea";
 import MdView from "components/MdView";
-import CustomMarked from "utils/CustomMarked";
+import parse from "utils/CustomMarked";
 
 const MdEditor: React.FC = () => {
 
-  const [md, setMd] = useState("");
+  const [md, setMd] = useState(
+`An h1 header
+============
+
+Paragraphs are separated by a blank line.
+
+2nd paragraph. *Italic*, **bold**, \`monospace\`. Itemized lists
+look like:
+
+  * this one
+  * that one
+
+> Block quotes are
+> written like so.
+
+An h2 header
+------------
+
+~~~
+define foobar() {
+    print 'Welcome to flavor country!';
+}
+~~~
+
+\`\`\`python
+import time
+# Quick, count to ten!
+for i in range(10):
+    # (but not *too* quick)
+    time.sleep(0.5)
+    print i
+\`\`\`
+
+### An h3 header ###
+
+Tables can look like this:
+
+|size |material     |color
+|---- |------------ |------------
+|9    |leather      |brown
+|10   |hemp canvas  |natural
+|11   |glass        |transparent
+
+### 日本語ヘッダー
+
+ネスト構造
+
+- ほげ
+  - ほげほげ
+- ぴよ
+  - ぴよぴよ`);
   const onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>): void => setMd(event.target.value);
 
   return (
@@ -15,7 +65,7 @@ const MdEditor: React.FC = () => {
         <MdTextarea value={md} setValue={onChangeHandler}></MdTextarea>
       </div>
       <div className="md-editor__block">
-        <MdView html={CustomMarked.parse(md)}></MdView>
+        <MdView html={parse(md)}></MdView>
       </div>
     </div>
   );
--- src/utils/CustomMarked.ts
+++ src/utils/CustomMarked.ts
@@ -1,21 +1,129 @@
-import marked from "marked";
+import marked, { Renderer } from "marked";
 import highlightjs from "highlight.js";
 
-class CustomMarked {
-  static parse(mdString: string): string {
-    marked.setOptions({
-      highlight: function(code, lang) {
-        return highlightjs.highlightAuto(code, [lang]).value;
-      },
-      pedantic: false,
-      gfm: true,
-      breaks: true,
-      sanitize: true,
-      silent: false
-    });
+const renderer = new Renderer();
 
-    return marked(mdString);
-  }
+// # text renderer
+renderer.heading = (text: string, level: number): string => {
+
+  const date = new Date();
+  const dateString = `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`
+  const datetimeDiv = level === 1 ? `<div class="datetime">更新日: ${dateString}</div>` : "";
+
+  return `
+    <h${level}>
+      <div>${text}</div>
+      ${datetimeDiv}
+    </h${level}>
+    ${ level === 1 ? "<hr>" : "" }
+  `;
 }
 
-export default CustomMarked;
+// **text** renderer
+renderer.strong = (text: string): string => {
+  return `
+    <strong>${text}</strong>
+  `;
+}
+
+// `text` renderer
+renderer.codespan = (code: string): string => {
+  return `
+    <code>${code}</code>
+  `;
+}
+
+// ```lang
+// code block
+// ```
+renderer.code = (code: string, lang: string) => {
+  const body = highlightjs.highlightAuto(code, [lang]).value;
+  
+  return `
+    <pre class="lang-${lang || "plaintext"}">${body}</pre>
+  `;
+}
+
+// p
+renderer.paragraph = (text: string): string => {
+  return `
+    <p>${text}</p>
+  `;
+}
+
+renderer.text = (text: string) => {
+  return `
+    <span>${text}</span>
+  `;
+}
+
+// list
+renderer.list = (body: string): string => {
+  return `
+    <ul>${body}</ul>
+  `;
+}
+
+renderer.listitem = (text: string): string => {
+  const listItems = text.split('<br>');
+  const listItem = (listItems.shift() || "").replace(/\n/g, "");
+  const subItem = (listItem.match(/<[^span]*>.*/) || [""])[0];
+  return `
+    <li>
+      ${listItem.replace(subItem, "")}
+    </li>
+    ${listItems.length === 0 ? "" : `<p>${listItems.join("<br>")}</p>`}
+    ${subItem}
+  `;
+}
+
+// > blockquote
+renderer.blockquote = (quote: string): string => {
+  return `
+    <blockquote>${quote}</blockquote>
+  `;
+}
+
+// table
+renderer.table = (header: string, body: string) => {
+  return `
+    <table>
+      <thead>
+        ${header}
+      </thead>
+      <tbody>
+        ${body}
+      </tbody>
+    </table>
+  `;
+}
+
+renderer.tablerow = (content: string) => {
+  return `
+    <tr>${content}</tr>
+  `;
+}
+
+renderer.tablecell = (content: string, flags: { header: boolean, align: string | null }): string => {
+  return `
+    ${ flags.header ? `<th>${content}</th>` : `<td>${content}</td>` }
+  `;
+}
+
+const parse = (mdString: string): string => {
+
+  marked.setOptions({
+    langPrefix: 'lang-',
+    headerIds: false,
+    pedantic: false,
+    gfm: true,
+    breaks: true,
+    sanitize: false,
+    silent: false,
+    renderer: renderer
+  });
+
+  return marked(mdString);
+}
+
+export default parse;
